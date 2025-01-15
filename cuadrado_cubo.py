24c2b01e23c2989917facda0d2a8f5f8d7846026
XandersJasso16 <2010115@utmatamoros.edu.mx>
2025-01-15 17:03:14 -0600

    Add files via upload

---
+++ cuadrado_cubo.py
@@ -0,0 +1,12 @@
+# cuadrado_cubo.py
+
+# Solicitar al usuario que introduzca un número
+numero = float(input("Introduce un número: "))
+
+# Calcular el cuadrado y el cubo
+cuadrado = numero ** 2
+cubo = numero ** 3
+
+# Mostrar los resultados
+print(f"El cuadrado de {numero} es {cuadrado}")
+print(f"El cubo de {numero} es {cubo}")
